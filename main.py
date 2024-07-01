7b318399db978233224cfb80d0aedf7307c04877
UdanthaH <uherath@ivytech.edu>
2024-06-30 23:00:39 -0400

    things to do 11.1-11.2

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+import zoo
+
+# Call the hours function from zoo module
+zoo.hours()
+
+# Import the zoo module as menagerie and call its hours function
+import zoo as menagerie
+menagerie.hours()
